9c213b3a5ff93cd88d0b0f0421266dfc50251589
EUVIT\radek <radek@euvit.cz>
2018-01-10 20:17:08 +0100

    výchozí verze

---
+++ app/build/generated/source/buildConfig/productionMode/debug/com/euvit/android/english/classic/czech/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.euvit.android.english.classic.czech;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.euvit.android.english.classic.czech";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "productionMode";
+  public static final int VERSION_CODE = 111;
+  public static final String VERSION_NAME = "1.5.0.111";
+}
